2157592ce61f1ba09018d35345f089c2d557eac5
Divino Borges <divino100rv@gmail.com>
2020-10-26 20:43:04 -0300

    docs: change the README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Algorithms
+
+Sorting algorithms | Python
+----------|-------
+[Insertion Sort](https://en.wikipedia.org/wiki/Insertion_sort) | [.py](https://github.com)
--- src/Python/main.py
+++ src/Python/main.py
@@ -1,4 +1,4 @@
-from insertion_sort import insertion_sort
+from src.Python.sorting_algorithms.insertion_sort import insertion_sort
 
 if __name__ == '__main__':
     sequence = [8, 9, 7, 6, 3, 2, 1, 5, 4, 0]
